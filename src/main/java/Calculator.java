962a17596f329437504a4fdffd7d7eaee3fe4e3d
EAusman <emmaausman@gmail.com>
2023-02-21 21:33:59 -0700

    implement

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,88 @@
+import java.util.UUID;
+
+class Calculator {
+
+    Calculator(){
+
+    }
+
+    int add(int a , int b){
+        return a + b;
+    }
+
+    int subtract(int a , int b){
+        return a - b;
+    }
+
+    int multiply(int a , int b){
+        return a * b;
+    }
+
+    int divide(int a , int b){
+        return a / b;
+    }
+
+
+    /*
+    Returns the n'th number in the fibonacci sequence
+    https://en.wikipedia.org/wiki/Fibonacci_number
+    Example below
+    n = x
+    0 = 0
+    1 = 1
+    2 = 1
+    3 = 2
+    4 = 3
+    5 = 5
+    .
+    .
+    .
+    etc
+     */
+    int fibonacciNumberFinder(int n){
+        int firstTerm = 0, secondTerm = 1;
+
+        for (int i = 1; i <= n; ++i) {
+            int nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
+        return secondTerm;
+    }
+
+
+    /*
+    Returns binary value of the given int number
+    The binary number will be stored as a string
+    if int a = 0 then this method returns: 0
+    if int a = 10 then this method returns: 1010
+    if int a = 16 then this method returns: 10000
+     */
+    String intToBinaryNumber(int n){
+
+        String binary = "";
+        int id = 0;
+
+        // Number should be positive
+        while (n > 0) {
+            binary =  binary + n % 2;
+            n = n / 2;
+        }
+        return binary;
+    }
+
+    /*
+    Create a completely unique String identifier for a given string
+    Each createdID must contain the string n in its unaltered Form
+    if String n = "Jason"
+    then the created ID could be = Jasonklfgn3jknnvksdfm - Because it contains the unaltered String n and is unique
+
+    if you run this function twice with the same String input, it must return 2 unique String IDs
+     */
+    String createUniqueID(String n){
+        UUID uuid = UUID.randomUUID();
+        return uuid + n;
+    }
+
+
+}
