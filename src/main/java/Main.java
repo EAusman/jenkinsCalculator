82981737f79655bb8caf9f764713438131e8f154
EAusman <emmaausman@gmail.com>
2023-03-02 22:26:58 -0700

    Main works

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,34 @@
+import java.io.Console;
+
 public class Main {
     public static void main(String args[]){
         Calculator calc = new Calculator();
-        if(args[0]=="add"){
-            System.out.println(calc.add(Integer.valueOf(args[1]),Integer.valueOf(args[2])));
+        String input = "";
+        System.out.println("Welcome to the console");
+        System.out.println("Enter a command:");
+        Console console = System.console();
+        while(input!="quit"){
+            input = console.readLine();
+            String[] words = input.split(" ");
+            if(words[0]=="add"){
+                System.out.println(calc.add(Integer.valueOf(words[1]),Integer.valueOf(words[2])));
+            }
+            else if(words[0]=="subtract"){
+                System.out.println(calc.subtract(Integer.valueOf(words[1]),Integer.valueOf(words[2])));
+            }
+            else if(words[0]=="multiply"){
+                System.out.println(calc.multiply(Integer.valueOf(words[1]),Integer.valueOf(words[2])));
+            }
+            else if(words[0]=="divide"){
+                System.out.println(calc.divide(Integer.valueOf(words[1]),Integer.valueOf(words[2])));
+            }
+            else if(words[0]=="fibonacciNumberFinder"){
+                System.out.println(calc.fibonacciNumberFinder(Integer.valueOf(words[1])));
+            }
+            else if(words[0]=="binary"){
+                System.out.println(calc.intToBinaryNumber(Integer.valueOf(words[1])));
+            }
         }
-        else if(args[0]=="subtract"){
-            System.out.println(calc.subtract(Integer.valueOf(args[1]),Integer.valueOf(args[2])));
-        }
-        else if(args[0]=="multiply"){
-            System.out.println(calc.multiply(Integer.valueOf(args[1]),Integer.valueOf(args[2])));
-        }
-        else if(args[0]=="divide"){
-            System.out.println(calc.divide(Integer.valueOf(args[1]),Integer.valueOf(args[2])));
-        }
-        else if(args[0]=="fibonacciNumberFinder"){
-            System.out.println(calc.fibonacciNumberFinder(Integer.valueOf(args[1])));
-        }
-        else if(args[0]=="binary"){
-            System.out.println(calc.intToBinaryNumber(Integer.valueOf(args[1])));
-        }
+
     }
 }
